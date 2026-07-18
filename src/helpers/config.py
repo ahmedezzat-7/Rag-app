60759e19c91acf79c2957fdd6fcaa1cdbba7a242
Ahmed Ezzat <ahmedezzatzezo7@gmail.com>
2026-07-18 17:07:13 +0300

    update

---
+++ src/helpers/config.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from pydantic_settings import BaseSettings, SettingsConfigDict 
+
+class Settings(BaseSettings):
+
+    APP_NAME: str
+    APP_VERSION: str
+    OPENAI_API_KEY: Optional[str] = None
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int
+    
+    
+    class Config:
+        env_file = ".env"
+
+def   get_settings():
+    return Settings()        
