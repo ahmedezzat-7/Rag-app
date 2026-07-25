9da3d01b889b9131c080307bb16f9b1121a473f4
Ahmed Ezzat <ahmedezzatzezo7@gmail.com>
2026-07-26 01:49:26 +0300

    update

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,9 +6,17 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: Optional[str] = None
+
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
+
+
+    MONGODB_URL:str
+    MONGODB_DATABASE:str
+
+
+
     
     
     class Config:
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from .enums.ResponseEnums import ResponseSignal
 
 from .enums.ProcessingEnum import ProcessingEnum
+
