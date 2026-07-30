213086db1cbac4047a34aaa0cc66d07e6bd15d2c
Ahmed Ezzat <ahmedezzatzezo7@gmail.com>
2026-07-31 00:56:12 +0300

    update

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,5 @@ from .enums.ResponseEnums import ResponseSignal
 from .enums.ProcessingEnum import ProcessingEnum
 
 from .enums.DataBaseEnum import DataBaseEnum
+
+from .enums.AssetTypeEnum import AssetTypeEnum
--- src/routes/data.py
+++ src/routes/data.py
@@ -9,7 +9,9 @@ import logging
 from .schemes.data import ProcessRequest
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunkModel
-from models.db_schemes import DataChunk
+from models.AssetModel import AssetModel
+from models.db_schemes import DataChunk , Asset
+from models.enums.AssetTypeEnum import AssetTypeEnum
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -65,12 +67,28 @@ async def upload_data(request: Request, project_id: str, file: UploadFile,
             }
         )
 
+    # store the assets into the database
+    asset_model = await AssetModel.create_instance(
+        db_client=request.app.db_client
+    )
+
+    asset_resource = Asset(
+        asset_project_id=project.id,
+        asset_type=AssetTypeEnum.FILE.value,
+        asset_name=file_id,
+        asset_size=os.path.getsize(file_path)
+    )
+
+    asset_record = await asset_model.create_asset(asset=asset_resource)
+
+
     return JSONResponse(
             content={
                 "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-                "file_id": file_id,
+                "file_id": str(asset_record.id),
             }
         )
+
 
 @data_router.post("/process/{project_id}")
 async def process_endpoint(request: Request, project_id: str, process_request: ProcessRequest):
@@ -88,6 +106,22 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
         project_id=project_id
     )
 
+    # project_files_ids = []
+    # if process_request.file_id:
+    #     project_files_ids = [process_request.file_id]
+    # else:
+    #     asset_model = await AssetModel.create_instance(
+    #         db_client=request.app.db_client
+    #     )    
+
+    #     project_files = await AssetModel.get_all_project_assets(
+    #         asset_project_id= project_id,
+    #         asset_type=AssetTypeEnum.FILE.value
+    #     )
+
+        
+
+    
     process_controller = ProcessController(project_id=project_id)
 
     file_content = process_controller.get_file_content(file_id=file_id)
