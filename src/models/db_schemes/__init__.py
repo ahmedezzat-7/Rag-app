9b0b1da5d5802c69d11309066d440337f9fb2f03
Ahmed Ezzat <ahmedezzatzezo7@gmail.com>
2026-09-02 20:32:07 +0300

    sqlalcamy and postgres

--- src/models/db_schemes/__init__.py
+++ src/models/db_schemes/__init__.py
@@ -1,4 +1,1 @@
-from .project import Project
-from .data_chunk import DataChunk,RetrievedDocument
-from .asset import Asset
-
+from models.db_schemes.minirag.schemes import DataChunk, Asset, Project, RetrievedDocument
